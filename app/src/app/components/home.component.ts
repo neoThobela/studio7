0efbe9d533a2a7b4bebaa5be29799d5068c69108
Neo <neo.thobela0002@gmail.com>
2022-06-05 15:30:25 +0200

    first commit

---
+++ app/src/app/components/home.component.ts
@@ -0,0 +1,97 @@
+/*DEFAULT GENERATED TEMPLATE. DO NOT CHANGE CLASS NAME*/
+//CORE_REFERENCE_IMPORTS
+//append_imports_start
+
+import {
+  Component,
+  Injector,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core'; //_splitter_
+import { SDBaseService } from 'app/n-services/SDBaseService'; //_splitter_
+import { SDPageCommonService } from 'app/n-services/sd-page-common.service'; //_splitter_
+import { __NEU_ServiceInvokerService__ } from 'app/n-services/service-caller.service'; //_splitter_
+//append_imports_end
+
+@Component({
+  selector: 'bh-home',
+  templateUrl: './home.template.html',
+  providers: [
+    //appendnew_element_providers
+  ],
+})
+export class homeComponent {
+  page: any = { dep: {} };
+  constructor(
+    private __page_injector__: Injector,
+    private sdService: SDBaseService,
+    public __serviceInvoker__: __NEU_ServiceInvokerService__
+  ) {
+    this.__page_injector__.get(SDPageCommonService).addPageDefaults(this.page);
+    this.registerListeners();
+    //appendnew_element_inject
+  }
+
+  ngOnInit() {
+    const bh: any = this.__page_injector__
+      .get(SDPageCommonService)
+      .constructFlowObject(this);
+    {
+      this.sd_aPSHGymf9A9nrv9H(bh);
+    }
+  }
+
+  private registerListeners() {
+    let bh = this.__page_injector__
+      .get(SDPageCommonService)
+      .constructFlowObject(this);
+
+    //append_listeners
+  }
+
+  sd_aPSHGymf9A9nrv9H(bh) {
+    try {
+      bh = this.sd_iMnFx5spMH8UTF0b(bh);
+      //appendnew_next_sd_aPSHGymf9A9nrv9H
+      return bh;
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_aPSHGymf9A9nrv9H');
+    }
+  }
+
+  //appendnew_flow_homeComponent_start
+
+  sd_iMnFx5spMH8UTF0b(bh) {
+    try {
+      //appendnew_next_sd_iMnFx5spMH8UTF0b
+      return bh;
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_iMnFx5spMH8UTF0b');
+    }
+  }
+
+  //appendnew_node
+
+  ngOnDestroy() {
+    const bh: any = this.__page_injector__
+      .get(SDPageCommonService)
+      .constructFlowObject(this);
+    this.__page_injector__.get(SDPageCommonService).deletePageFromMap(this);
+  }
+
+  errorHandler(bh, e, src) {
+    console.error(e);
+    bh.error = e;
+    bh.errorSource = src;
+    if (
+      false
+      /*appendnew_next_Catch*/
+    ) {
+      return bh;
+    } else {
+      throw e;
+    }
+  }
+  //appendnew_flow_homeComponent_Catch
+}
